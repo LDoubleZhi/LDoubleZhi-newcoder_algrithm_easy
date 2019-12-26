39f54a430a31d69028033d263938096b7b22f9fb
LDoubleZhi <email@example.com>
2019-12-26 14:20:55 +0800

    add GameLauncher

--- chapter1/demo/src/HelloWorld.java
+++ chapter1/demo/src/HelloWorld.java
@@ -1,9 +1,13 @@
-package PACKAGE_NAME;
-
 /**
  * @author : liulinzhi
  * @date: 2019/12/25/19:55
  * @description:
  */
 public class HelloWorld {
+    int size = 27;
+    String name = "Fido";
+    Dog myDog = new Dog(name, size);
+    x = size - 5;
+    if (x )
+
 }
